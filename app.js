2c9f5215903925b1ca4483c3e0be7a86d9bcd2f2
Oliver Kuopus <141285426+oakuopus@users.noreply.github.com>
2024-10-03 15:14:15 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,94 +1,152 @@
-//Call all modules used
 const express = require("express")
-const bodyParser = require("body-parser")
-const fs = require("fs")
-const app = express()
 const path = require("path")
+const app = express()
+const fs = require("fs")
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
-//middleware
-app.use(bodyParser.urlencoded({extended: true})) //parses response object data 
-app.use(express.static("public")) //declares css folder as "public"
-app.set("view engine", "ejs") //sets view engine to ejs
+const getData1 = () => {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "db", "data1.json"), "utf-8"))
+}
+const getData2 = () => {
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "db", "data2.json"), "utf-8"))
+}
 
-//load tasks from json file with fs and json
-const getTasks = () => {
-    const data = fs.readFileSync("./data/tasks.json", "utf8")
-    return JSON.parse(data)
-}
-// saves tasks to tasks file, using fs and json stringify
-const saveTasks = (tasks) => {
-    fs.writeFileSync("./data/tasks.json", JSON.stringify(tasks, null, 2))
-}
-// appends event registered to data.txt file with fs and json
-const regEvent = (event) => {
-    fs.appendFileSync("./data/data.txt", JSON.stringify(event, null, 2))
-}
-//Routes
-
-//get: show all tasks on admin page
 app.get("/", (req, res) => {
-    const tasks = getTasks() //declares var tasks as getTasks func
-    res.render("admin", {tasks}) //renders admin page with tasks object
+    res.sendFile(path.resolve(__dirname, "public/index.html"))
 })
 
-//show all tasks on user page
-app.get("/events", (req, res) =>{
-    const tasks = getTasks() 
-    res.render("events", {tasks}) 
+app.get('/api/data', (req, res) => {
+    const allData = [...getData1(), ...getData2()]
+    res.json(allData)
 })
 
-//post: make new task
-app.post("/tasks", (req, res) =>{
-    const tasks = getTasks()
-    const newTask = { //create new task object 
-        id: tasks.length+1, //add 1 to current highest id
-        name: req.body.name, //declares name, date, desc with info from form
-        date: req.body.date,
-        description: req.body.description
+const addEntry = (entry, file) => {
+    const append = (file == "1") ? getData1() : getData2();
+    append.push(entry)
+    fs.writeFileSync(`./db/data${file}.json`, JSON.stringify(append, null, 2));
+}
+const saveData = (data, file) => {
+    fs.writeFileSync(`./db/data${file}.json`, JSON.stringify(data, null, 2))
+}
+
+app.get("/admin", (req, res) => {
+    if(req.query.password == "admin"){
+        res.sendFile(path.resolve(__dirname, "public/admin.html"))
+    }else{
+        res.status(401).send("Unauthorized");
     }
-    tasks.push(newTask) // push new task object to task func, into tasks.json
-    saveTasks(tasks) //update task variable 
-    res.redirect("/") //redirect to reload page and see update
 })
 
-//register for event
-app.post("/events", (req, res) =>{
-    const reg = { // create new user registration object and repeat above steps, pushing to data.txt file
-        EventId: req.body.id,
-        name: req.body.name,
-        email: req.body.email
+app.get('/api/data/cars', (req, res) => {
+    res.json(getData1())
+})
+app.get('/api/data/motorcycles', (req, res) => {
+    res.json(getData2())
+})
+
+app.get("/api/data/cars/:dataID", (req, res) => {
+    const { dataID } = req.params;
+    const single = getData1().find(product => product.id == dataID);
+    if (single) {
+        res.json(single);
+    } else {
+        res.status(404).send("Entry not found");
     }
-    regEvent(reg)
-    res.redirect("/events")
+})
+app.get("/api/data/motorcycles/:dataID", (req, res) => {
+    const { dataID } = req.params;
+    const single = getData2().find(product => product.id == dataID);
+    if (single) {
+        res.json(single);
+    } else {
+        res.status(404).send("Entry not found");
+    }
 })
 
-//Update any task
-app.get("/tasks/:id/edit", (req, res)=>{
-    const tasks = getTasks()
-    const task = tasks.find(task => task.id == req.params.id) //use .find to pull task with matching id as user input
-    res.render("tasks", {task}) //render tasks page with specified task
+app.post('/entry', (req, res) => {
+    var file = req.body.vehicle == "Car" ? "1" : "2";
+        if(file == "1"){
+            const newEntry = {
+                id: getData1()[-1].id + 1,
+                model: req.body.model,
+                year: req.body.year,
+            }
+            addEntry(newEntry, "1")
+        }else if(file == "2")  {
+            const newEntry = {
+                id: getData2()[-1].id + 1,
+                model: req.body.model,
+                year: req.body.year,
+            }
+            addEntry(newEntry, "2")
+        }else{
+            res.status(401).send("Invalid")
+        }
+    res.redirect("/admin?password=admin")
 })
 
-//PUT: view any task
-app.post("/tasks/:id", (req, res) =>{
-    const tasks = getTasks()
-    const taskIndex = tasks.findIndex(task => task.id == req.params.id) // pull task with findIndex to view specific task
-    tasks[taskIndex].description = req.body.description
-    tasks[taskIndex].name = req.body.name
-    tasks[taskIndex].date = req.body.date
-    saveTasks(tasks)
-    res.redirect("/")
+app.get("/admin/edit/cars/:id", (req, res) => {
+    var edit = getData1().find(edit => edit.id == Number(req.params.id))
+    if(!req.params.id){
+        res.status(404).send("ID not found")
+    }
+    if(req.query.password == "admin") {
+        res.render("edit", {edit})
+    }else{
+        res.status(401).send("Unauthorized")
+    }
+})
+app.get("/admin/edit/motorcycles/:id", (req, res) => {
+    var edit = getData2().find(edit => edit.id == Number(req.params.id))
+    if(!req.params.id){
+        res.status(404).send("ID not found")
+    }
+    if(req.query.password == "admin") {
+        res.render("edit", {edit})
+    }else{
+        res.status(401).send("Unauthorized")
+    }
 })
 
-//delete a task
-app.post("/tasks/:id/delete", (req, res) =>{
-    let tasks = getTasks()
-    tasks = tasks.filter(task => task.id != req.params.id) //filter out all tasks that do not have matching id
-    saveTasks(tasks) //save all filtered tasks
-    res.redirect("/")
+app.post("/admin/:id", (req, res) =>{
+    var edit = getData1()
+    var file = req.body.vehicle == "Car" ? "1" : "2";
+    console.log(file)
+    var editIndex = edit.findIndex(edit => edit.id == req.params.id)
+    edit[editIndex].model = req.body.model
+    edit[editIndex].year = req.body.year
+    if(file == "1"){
+        saveData(edit, "1")
+    }else{
+        saveData(edit, "2")
+    }
+    res.redirect("/admin?password=admin")
 })
 
-// listen on server 5000 and respond when listening
-app.listen(5000, ()=>{
-    console.log(`Listening on port ${5000}`)
+app.post("/admin/edit/cars/:id/delete", (req, res) => {
+    let data = getData1()
+    console.log(req.params.id)
+    data = data.filter(item => item.id != req.params.id)
+    saveData(data, "1")
+    res.redirect("/admin?password=admin")
+})
+
+app.post("/admin/edit/motorcycles/:id/delete", (req, res) => {
+    let data = getData2()
+    data = data.filter(del => del.id != req.params.id)
+    saveData(data, "2")
+    res.redirect("/admin?password=admin")
+})
+
+app.get("*", (req, res) => {
+    res.status(404).send("404 you cannot use this route.")
+})
+
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000")
 })
